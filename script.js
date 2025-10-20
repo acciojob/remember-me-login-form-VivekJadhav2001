6c57b73a55ff640f7cc57a9bf20e01e329afd76f
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-20 10:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+        const userName = document.getElementById("username");
+        const password = document.getElementById("password");
+        const checkbox = document.getElementById("checkbox");
+        const existingBtn = document.getElementById("existing");
+
+        existingBtn.style.display = "none";
+
+        if (localStorage.getItem("username") && localStorage.getItem("password")) {
+            existingBtn.style.display = "inline-block";
+        }
+
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const isChecked = checkbox.checked;
+
+            alert(`Logged in as ${userName.value}`);
+
+            if (isChecked) {
+                localStorage.setItem("username", userName.value);
+                localStorage.setItem("password", password.value);
+                existingBtn.style.display = "inline-block";
+            } else {
+                localStorage.removeItem("username");
+                localStorage.removeItem("password");
+                existingBtn.style.display = "none";
+            }
+        });
+
+        existingBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            alert(`Logged in as ${localStorage.getItem("username")}`);
+        });
